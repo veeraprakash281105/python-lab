05b740df9a65c06fb82581b86e95efe38a1d7c13
veeraprakash281105 <veeraprakash2811@gmail.com>
2026-03-10 12:29:45 +0530

    Create change the letters.py

---
+++ EX9/change the letters.py
@@ -0,0 +1,6 @@
+s1=input("Enter string 1:")
+s2=input("Enter string 2:")
+s3=s2[:2]+s1[2:]
+s4=s1[:2]+s2[2:]
+print(f"the input string is: {s1} {s2}")
+print(f"the value changed string is: {s3} {s4}")
